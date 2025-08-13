0110a99501c5e9ef172b03e7146e492352cfad44
Devansh <devanshmittal9b@gmail.com>
2025-08-13 18:15:35 +0530

    Java initials

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) //For employee
+    {
+        Employee e1 = new Employee(1,"Devansh");
+        e1.id = 12;
+        e1.display();
+
+    }
+}
---
+++ src/Student.java
@@ -0,0 +1,17 @@
+public class Student
+{
+    int roll_no; //Data-member
+    String name; //Data-member
+    Student() //Default Constructor
+    {
+        System.out.println("I am default constructor");
+    }
+    Student(int r, String n) //Constructor
+    {
+        roll_no = r;
+        name = n;
+    }
+    public void display(){
+        System.out.println("Roll no. "+roll_no+" Name "+name);
+    }
+}
